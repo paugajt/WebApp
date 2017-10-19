066225418cb76edc5872023ce4ea63c64fe9b1a6
Justin Pauga <paugajt@gmail.com>
2017-10-18 18:58:22 -0600

    changed application layout to fix errors

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,11 @@
 class Student < ApplicationRecord
 	has_and_belongs_to_many :sections
-	
+  validates :name, uniqueness: true
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :name, length: {minimum: 2}
+  validates :credits, length: {minimum: 0, maximum: 200}
+  validates_format_of :email,:with => Devise::email_regexp
+
+
 end
